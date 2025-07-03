1840f579b8fef786cd9cae92b6dc6f9a84336ab5
Rajdeep Agrawal <rajdeepagrawal015@gmail.com>
2025-07-03 20:30:17 +0530

    Add Footer page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import About from './components/About'
 import Art from './components/Art'
 import Cocktails from './components/Cocktails'
+import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Menu from './components/Menu'
 import Navbar from './components/Navbar'
@@ -19,6 +20,7 @@ const App = () => {
       <About />
       <Art />
       <Menu />
+      <Footer/>
     </main>
 
   )
